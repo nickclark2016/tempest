c8a142a9a7260caf22a32ca011d957e5fe8b3121
Nicholaus Clark <nicholaus.clark115@gmail.com>
2023-04-30 14:44:22 -0400

    Added value iteration for sparse map

--- projects/core/include/tempest/algorithm.hpp
+++ projects/core/include/tempest/algorithm.hpp
@@ -3,6 +3,7 @@
 
 #include <iterator>
 #include <memory>
+#include <type_traits>
 
 namespace tempest::core
 {
@@ -19,6 +20,34 @@ namespace tempest::core
         }
 
         return d_first;
+    }
+
+    template <typename InputIt, typename OutputIt>
+    inline constexpr OutputIt move_construct(InputIt first, InputIt last, OutputIt d_first)
+    {
+        using src_traits = std::iterator_traits<InputIt>;
+        using dst_traits = std::iterator_traits<OutputIt>;
+
+        for (; first != last; (void)++first, (void)++d_first)
+        {
+            typename dst_traits::pointer ptr = std::addressof(*d_first);
+            std::construct_at(ptr, std::move(*first));
+        }
+
+        return d_first;
+    }
+
+    template <typename InputIt, typename OutputIt>
+    inline constexpr OutputIt optimal_construct(InputIt first, InputIt last, OutputIt d_first)
+    {
+        if constexpr (std::is_move_constructible_v<typename std::iterator_traits<OutputIt>::value_type>)
+        {
+            return move_construct(first, last, d_first);
+        }
+        else
+        {
+            return copy_construct(first, last, d_first);
+        }
     }
 } // namespace tempest::core
 
