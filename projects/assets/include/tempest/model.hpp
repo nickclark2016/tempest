5a7e37bd7f074d927da2a6ae8339070782b85ced
Nicholaus Clark <nicholaus.clark115@gmail.com>
2023-05-11 20:19:14 +0200

    Slight refactor on model loading

--- projects/assets/include/tempest/model.hpp
+++ projects/assets/include/tempest/model.hpp
@@ -1,6 +1,10 @@
 #ifndef tempest_model_hpp
 #define tempest_model_hpp
 
+#include "tempest/model.hpp"
+#include <tempest/mat.hpp>
+#include <tempest/vertex.hpp>
+
 #include <vector>
 #include <string>
 #include <memory>
@@ -10,6 +14,44 @@ namespace tempest::assets
     class model
     {
       public:
+        struct primitive
+        {
+            std::uint32_t first_index;
+            std::uint32_t index_count;
+            std::int32_t material_index;
+        };
+
+        struct mesh
+        {
+            std::vector<primitive> primitives;
+        };
+
+        struct node
+        {
+            node* parent = nullptr;
+            std::vector<node*> children;
+            mesh* m = nullptr;
+            tempest::math::mat<float, 4, 4> matrix;
+            std::string name;
+            bool visibile = true;
+
+            node() : matrix(1.0f)
+            {
+            }
+
+            ~node()
+            {
+                for (auto& child : children)
+                {
+                    delete child;
+                }
+            }
+        };
+
+        node* root = nullptr;
+        std::vector<core::vertex> vertices;
+        std::vector<std::uint32_t> indices;
+
         virtual bool load_from_binary(const std::vector<unsigned char>& binary_data) = 0;
         virtual bool load_from_ascii(const std::string& ascii_data) = 0;
     };
