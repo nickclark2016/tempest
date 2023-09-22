2184491037f5359581bb720b7ee451f53cb49a82
Nicholaus Clark <nicholaus.clark115@gmail.com>
2023-09-21 20:44:08 -0400

    Updated build to automatically find vswhere for building

--- build.bat
+++ build.bat
@@ -1,3 +1,15 @@
+@echo off
+
 .\dependencies\conjure\conjure.exe ninja
-ninja -f Tempest.ninja Debug_x64
-ninja -f Tempest.ninja Release_x64
+
+for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+    set InstallDir=%%i
+)
+
+if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
+    "%InstallDir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64
+    ninja -f Tempest.ninja Debug_x64
+    ninja -f Tempest.ninja Release_x64
+) else (
+    echo "Faied to find vswhere"
+)
