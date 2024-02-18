b1754357bc18dc9324a12e1a5c83185cc003a171
Nicholaus Clark <nicholaus.clark115@gmail.com>
2024-02-18 17:48:14 -0500

    Entity modification functions for registry

--- projects/ecs/tests/registry_test.cpp
+++ projects/ecs/tests/registry_test.cpp
@@ -186,3 +186,151 @@ TEST(registry, acquire_entity)
     ASSERT_TRUE(reg.is_valid(entity));
     ASSERT_EQ(reg.entity_count(), 1);
 }
+
+TEST(registry, assign_entity)
+{
+    registry reg;
+
+    const auto entity = reg.acquire_entity();
+
+    ASSERT_TRUE(reg.is_valid(entity));
+    ASSERT_EQ(reg.entity_count(), 1);
+
+    reg.assign<int>(entity, 42);
+
+    ASSERT_TRUE(reg.has<int>(entity));
+    ASSERT_FALSE(reg.has<float>(entity));
+    ASSERT_EQ(reg.get<int>(entity), 42);
+
+    reg.remove<int>(entity);
+    ASSERT_FALSE(reg.has<int>(entity));
+}
+
+TEST(registry, assign_multiple_entities_multiple_components)
+{
+    registry reg;
+
+    const auto entity1 = reg.acquire_entity();
+    const auto entity2 = reg.acquire_entity();
+
+    ASSERT_TRUE(reg.is_valid(entity1));
+    ASSERT_TRUE(reg.is_valid(entity2));
+    ASSERT_EQ(reg.entity_count(), 2);
+
+    reg.assign<int>(entity1, 42);
+    reg.assign<float>(entity1, 3.14f);
+
+    reg.assign<int>(entity2, 24);
+    reg.assign<float>(entity2, 6.28f);
+
+    ASSERT_TRUE(reg.has<int>(entity1));
+    ASSERT_TRUE(reg.has<float>(entity1));
+    ASSERT_EQ(reg.get<int>(entity1), 42);
+    ASSERT_EQ(reg.get<float>(entity1), 3.14f);
+
+    ASSERT_TRUE(reg.has<int>(entity2));
+    ASSERT_TRUE(reg.has<float>(entity2));
+    ASSERT_EQ(reg.get<int>(entity2), 24);
+    ASSERT_EQ(reg.get<float>(entity2), 6.28f);
+
+    reg.remove<int>(entity1);
+    reg.remove<float>(entity1);
+
+    reg.remove<int>(entity2);
+    reg.remove<float>(entity2);
+
+    ASSERT_FALSE(reg.has<int>(entity1));
+    ASSERT_FALSE(reg.has<float>(entity1));
+
+    ASSERT_FALSE(reg.has<int>(entity2));
+    ASSERT_FALSE(reg.has<float>(entity2));
+}
+
+TEST(registry, assign_multiple_entities_different_components)
+{
+    registry reg;
+
+    const auto entity1 = reg.acquire_entity();
+    const auto entity2 = reg.acquire_entity();
+
+    ASSERT_TRUE(reg.is_valid(entity1));
+    ASSERT_TRUE(reg.is_valid(entity2));
+    ASSERT_EQ(reg.entity_count(), 2);
+
+    reg.assign<int>(entity1, 42);
+    reg.assign<float>(entity2, 3.14f);
+
+    ASSERT_TRUE(reg.has<int>(entity1));
+    ASSERT_FALSE(reg.has<float>(entity1));
+    ASSERT_EQ(reg.get<int>(entity1), 42);
+
+    ASSERT_FALSE(reg.has<int>(entity2));
+    ASSERT_TRUE(reg.has<float>(entity2));
+    ASSERT_EQ(reg.get<float>(entity2), 3.14f);
+
+    reg.remove<int>(entity1);
+    reg.remove<float>(entity2);
+
+    ASSERT_FALSE(reg.has<int>(entity1));
+    ASSERT_FALSE(reg.has<float>(entity2));
+}
+
+TEST(registry, assign_large_entity_count)
+{
+    registry reg;
+
+    std::size_t entity_count = 16384;
+    std::vector<entity> entities;
+
+    for (std::size_t i = 0; i < entity_count; ++i)
+    {
+        const auto entity = reg.acquire_entity();
+        
+        if (i % 2 == 0)
+        {
+            reg.assign<int>(entity, 42);
+        }
+        else
+        {
+            reg.assign<float>(entity, 3.14f);
+        }
+
+        if (i % 4 < 2)
+        {
+            reg.assign<double>(entity, 6.28);
+        }
+        else
+        {
+            reg.assign<char>(entity, 'a');
+        }
+
+        entities.push_back(entity);
+    }
+
+    for (std::size_t i = 0; i < entity_count; ++i)
+    {
+        const auto entity = entities[i];
+
+        if (i % 2 == 0)
+        {
+            ASSERT_TRUE(reg.has<int>(entity));
+            ASSERT_EQ(reg.get<int>(entity), 42);
+        }
+        else
+        {
+            ASSERT_TRUE(reg.has<float>(entity));
+            ASSERT_EQ(reg.get<float>(entity), 3.14f);
+        }
+
+        if (i % 4 < 2)
+        {
+            ASSERT_TRUE(reg.has<double>(entity));
+            ASSERT_EQ(reg.get<double>(entity), 6.28);
+        }
+        else
+        {
+            ASSERT_TRUE(reg.has<char>(entity));
+            ASSERT_EQ(reg.get<char>(entity), 'a');
+        }
+    }
+}
