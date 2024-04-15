1755c81e6a35e24608643805f08ea35b7c96366b
Nicholaus Clark <nicholaus.clark115@gmail.com>
2024-04-15 10:28:59 -0400

    Add shaders to sandbox compile

--- sandbox/conjure.js
+++ sandbox/conjure.js
@@ -1,4 +1,4 @@
-project('sandbox', (_) => {
+project('sandbox', _ => {
     kind('ConsoleApp');
     language('C++');
 
@@ -10,6 +10,41 @@ project('sandbox', (_) => {
     uses([
         'tempest:engine'
     ]);
+
+    when({}, (ctx) => {
+        const slangPath = (() => {
+            if (ctx.system === 'windows') {
+                if (ctx.exporter === 'vstudio') { 
+                    return `../dependencies/slang/${ctx.system}/slangc.exe`;
+                } else {
+                    return `./dependencies/slang/${ctx.system}/slangc.exe`;
+                }
+            } else if (ctx.system === 'linux') {
+                return `./dependencies/slang/${ctx.system}/slangc`;
+            } else {
+                throw new Error(`Unsupported system: ${ctx.system}`);
+            }
+        })();
+
+        const shaderBasePath = ctx.exporter === 'vstudio' ? 'assets/shaders'  : 'sandbox/assets/shaders';
+        const debugFlags = ctx.configuration === 'Debug' ? '-g3 -O1 -line-directive-mode source-map' : '-O3';
+
+        const events = [
+            `${slangPath} ${shaderBasePath}/pbr.slang -target spirv -o ${shaderBasePath}/pbr.vert.spv -entry VSMain ${debugFlags}`,
+            `${slangPath} ${shaderBasePath}/pbr.slang -target spirv -o ${shaderBasePath}/pbr.frag.spv -entry FSMain ${debugFlags}`,
+            `${slangPath} ${shaderBasePath}/hzb.slang -target spirv -o ${shaderBasePath}/hzb.comp.spv -entry CSMain ${debugFlags}`,
+            `${slangPath} ${shaderBasePath}/zprepass.slang -target spirv -o ${shaderBasePath}/zprepass.vert.spv -entry VSMain ${debugFlags}`,
+            `${slangPath} ${shaderBasePath}/zprepass.slang -target spirv -o ${shaderBasePath}/zprepass.frag.spv -entry FSMain ${debugFlags}`,
+            `${slangPath} ${shaderBasePath}/taa.slang -target spirv -o ${shaderBasePath}/taa.vert.spv -entry VSMain ${debugFlags}`,
+            `${slangPath} ${shaderBasePath}/taa.slang -target spirv -o ${shaderBasePath}/taa.frag.spv -entry FSMain ${debugFlags}`
+        ];
+
+        for (let i = 0; i < events.length; i++) {
+            events[i] = events[i].replaceAll('/', '\\');
+        }
+
+        postBuildEvents(events);
+    });
 
     dependsOn(['tempest']);
 });
