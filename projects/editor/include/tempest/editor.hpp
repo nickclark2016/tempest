cca272761bb19dac6cd6b46e3453e9de72ac3ded
Nicholaus Clark <nicholaus.clark115@gmail.com>
2024-09-16 19:49:49 -0400

    Backed out changes for asset system for rework

--- projects/editor/include/tempest/editor.hpp
+++ projects/editor/include/tempest/editor.hpp
@@ -1,7 +1,6 @@
 #ifndef tempest_editor_editor_hpp
 #define tempest_editor_editor_hpp
 
-#include "asset_database_view.hpp"
 #include "entity_inspector_view.hpp"
 #include "hierarchy_view.hpp"
 
@@ -18,7 +17,6 @@ namespace tempest::editor
         ecs::entity selected_entity() const noexcept { return _hierarchy_view.selected_entity(); }
 
       private:
-        asset_database_view _asset_database_view;
         hierarchy_view _hierarchy_view;
         entity_inspector_view _entity_inspector_view;
     };
