ff553079e56a3086a26147d20a6105af25367559
Nicholaus Clark <nicholaus.clark115@gmail.com>
2025-01-03 17:50:37 -0500

    Optimizing assembly

--- projects/math/asm/clang/mul_mat4_mat4.s
+++ projects/math/asm/clang/mul_mat4_mat4.s
@@ -5,13 +5,7 @@
 # - The result pointer must be aligned to 16 bytes.
 # - The lhs and rhs pointers must be aligned to 16 bytes.
 # - The data pointers are assumed to be valid.
-# - The data pointers represent column major matrices
-
-.LCPI0_0:
-    .long   0                               # 0x0
-    .long   16                              # 0x10
-    .long   32                              # 0x20
-    .long   48                              # 0x30
+# - The data pointers represent row major matrices
 
     .file "mul_mat4_mat4.s"
     .intel_syntax noprefix
@@ -21,61 +15,31 @@
     .globl tempest_math_aligned_mul_mat4_mat4
     .type  tempest_math_aligned_mul_mat4_mat4, @function
 tempest_math_aligned_mul_mat4_mat4:
-    push    r15
-    push    r14
-    push    r12
-    push    rbx
+    vmovups xmm0, xmmword ptr [rsi]
+    vmovups xmm1, xmmword ptr [rsi + 16]
+    vmovups xmm2, xmmword ptr [rsi + 32]
+    vmovups xmm3, xmmword ptr [rsi + 48]
     xor     eax, eax
-    vmovdqa xmm0, xmmword ptr [rip + .LCPI0_0]
-.LBB0_1:
-    vmovd   xmm1, eax
-    vpslld  xmm1, xmm1, 2
-    vpshufd xmm2, xmm1, 0
-    vpor    xmm2, xmm2, xmm0
-    vmovd   ecx, xmm1
-    mov     dword ptr [rdx + rcx], 0
-    vpextrd r8d, xmm2, 1
-    mov     dword ptr [rdx + r8], 0
-    vpextrd r9d, xmm2, 2
-    vpextrd r10d, xmm2, 3
-    mov     dword ptr [rdx + r9], 0
-    mov     dword ptr [rdx + r10], 0
-    vpxor   xmm1, xmm1, xmm1
-    xor     r11d, r11d
-.LBB0_2:
-    vbroadcastss ymm2, dword ptr [rdi + 4*r11]
-    vmovd        xmm3, r11d
-    vpshufd      xmm3, xmm3, 0
-    vpor         xmm3, xmm3, xmm0
-    vpextrd      ebx, xmm3, 1
-    vpextrd      r14d, xmm3, 2
-    mov          r15d, r11d
-    vpextrd      r12d, xmm3, 3
-    vmovss       xmm3, dword ptr [rsi + r15] 
-    vinsertps    xmm3, xmm3, dword ptr [rsi + rbx], 16
-    vinsertps    xmm3, xmm3, dword ptr [rsi + r14], 32
-    vinsertps    xmm3, xmm3, dword ptr [rsi + r12], 48
-    vmovss       xmm4, dword ptr [rdx + rcx]
-    vinsertps    xmm4, xmm4, dword ptr [rdx + r8], 16
-    vinsertps    xmm4, xmm4, dword ptr [rdx + r9], 32
-    vinsertps    xmm1, xmm4, xmm1, 48
-    vmulps       xmm2, xmm2, xmm3
-    vaddps       xmm1, xmm2, xmm1
-    vmovss       dword ptr [rdx + rcx], xmm1
-    vextractps   dword ptr [rdx + r8], xmm1, 1
-    vextractps   dword ptr [rdx + r9], xmm1, 2
-    vshufps      xmm1, xmm1, xmm1, 255
-    vmovss       dword ptr [rdx + r10], xmm1
-    add          r11, 4
-    cmp          r11, 16
-    jne          .LBB0_2
-    inc          rax
-    add          rdi, 4
-    cmp          rax, 4
-    jne          .LBB0_1
-    pop          rbx
-    pop          r12
-    pop          r14
-    pop          r15
+    vxorps  xmm4, xmm4, xmm4
+.LBB0_1:                                # =>This Inner Loop Header: Depth=1
+    vbroadcastss    xmm5, dword ptr [rdi + rax]
+    vmulps  xmm5, xmm0, xmm5
+    vaddps  xmm5, xmm5, xmm4
+    vmovups xmmword ptr [rdx + rax], xmm5
+    vbroadcastss    xmm6, dword ptr [rdi + rax + 4]
+    vmulps  xmm6, xmm1, xmm6
+    vaddps  xmm5, xmm5, xmm6
+    vmovups xmmword ptr [rdx + rax], xmm5
+    vbroadcastss    xmm6, dword ptr [rdi + rax + 8]
+    vmulps  xmm6, xmm2, xmm6
+    vaddps  xmm5, xmm5, xmm6
+    vmovups xmmword ptr [rdx + rax], xmm5
+    vbroadcastss    xmm6, dword ptr [rdi + rax + 12]
+    vmulps  xmm6, xmm3, xmm6
+    vaddps  xmm5, xmm5, xmm6
+    vmovups xmmword ptr [rdx + rax], xmm5
+    add     rax, 16
+    cmp     rax, 64
+    jne     .LBB0_1
     vzeroupper
     ret
--- projects/math/asm/msc/mul_assign_mat4_mat4.s
+++ projects/math/asm/msc/mul_assign_mat4_mat4.s
@@ -13,119 +13,40 @@ tempest_math_aligned_assign_mul_mat4_mat4 PROC
     mov     rbp, rsp
     and     rsp, -32
     sub     rsp, 96
-    mov     rax, rsp
-
-    push    rdi
-    push    rsi
-
-    ; copy RCX -> RDI
-    ; copy RDX -> RSI
-    
-    mov rdi, rcx
-    mov rsi, rdx
-
-    vmovq   xmm0, rax
-    vpshufd xmm0, xmm0, 68                  
-    vinsertf128     ymm0, ymm0, xmm0, 1
-    xor     eax, eax
-    vextractf128    xmm1, ymm0, 1
-    vmovdqa xmm2, xmmword ptr [OFFSET LCPI0_0]
-    vmovdqa xmm3, xmmword ptr [OFFSET LCPI0_1]
+    vmovups xmm0, xmmword ptr [rdx]
+    vmovups xmm1, xmmword ptr [rdx + 16]
+    vmovups xmm2, xmmword ptr [rdx + 32]
+    vmovups xmm3, xmmword ptr [rdx + 48]
     xor     eax, eax
     vxorps  xmm4, xmm4, xmm4
-$LL0:
-    vmovd   xmm4, eax
-    vpshufd xmm4, xmm4, 0                  
-    vpslld  xmm4, xmm4, 2
-    vpmovzxdq       xmm4, xmm4             
-    vpor    xmm5, xmm1, xmm4
-    vpaddq  xmm5, xmm5, xmm2
-    vpor    xmm4, xmm0, xmm4
-    vpaddq  xmm4, xmm4, xmm3
-    vmovq   rcx, xmm4
-    mov     dword ptr [rcx], 0
-    vpextrq rdx, xmm4, 1
-    mov     dword ptr [rdx], 0
-    vmovq   r8, xmm5
-    vpextrq r9, xmm5, 1
-    mov     dword ptr [r8], 0
-    mov     dword ptr [r9], 0
-    vbroadcastss    ymm4, dword ptr [rdi + 4*rax]
-    vmovss  xmm5, dword ptr [rsi]           
-    vinsertps       xmm5, xmm5, dword ptr [rsi + 16], 16 
-    vinsertps       xmm5, xmm5, dword ptr [rsi + 32], 32 
-    vinsertps       xmm5, xmm5, dword ptr [rsi + 48], 48 
-    vmovss  xmm6, dword ptr [rcx]           
-    vinsertps       xmm6, xmm6, dword ptr [rdx], 28
-    vinsertps       xmm6, xmm6, dword ptr [r8], 40
-    vmulps  xmm4, xmm4, xmm5
-    vaddps  xmm4, xmm4, xmm6
-    vmovss  dword ptr [rcx], xmm4
-    vextractps      dword ptr [rdx], xmm4, 1
-    vextractps      dword ptr [r8], xmm4, 2
-    vshufps xmm5, xmm4, xmm4, 255           
-    vmovss  dword ptr [r9], xmm5
-    vbroadcastss    ymm5, dword ptr [rdi + 4*rax + 16]
-    vmovss  xmm6, dword ptr [rsi + 4]       
-    vinsertps       xmm6, xmm6, dword ptr [rsi + 20], 16 
-    vinsertps       xmm6, xmm6, dword ptr [rsi + 36], 32 
-    vinsertps       xmm6, xmm6, dword ptr [rsi + 52], 48 
-    vmovss  xmm7, dword ptr [rcx]           
-    vinsertps       xmm7, xmm7, dword ptr [rdx], 16
-    vinsertps       xmm7, xmm7, dword ptr [r8], 32 
-    vblendps        xmm4, xmm7, xmm4, 8            
-    vmulps  xmm5, xmm5, xmm6
-    vaddps  xmm4, xmm5, xmm4
-    vmovss  dword ptr [rcx], xmm4
-    vextractps      dword ptr [rdx], xmm4, 1
-    vextractps      dword ptr [r8], xmm4, 2
-    vshufps xmm5, xmm4, xmm4, 255          
-    vmovss  dword ptr [r9], xmm5
-    vbroadcastss    ymm5, dword ptr [rdi + 4*rax + 32]
-    vmovss  xmm6, dword ptr [rsi + 8]      
-    vinsertps       xmm6, xmm6, dword ptr [rsi + 24], 16
-    vinsertps       xmm6, xmm6, dword ptr [rsi + 40], 32
-    vinsertps       xmm6, xmm6, dword ptr [rsi + 56], 48
-    vmovss  xmm7, dword ptr [rcx]          
-    vinsertps       xmm7, xmm7, dword ptr [rdx], 16
-    vinsertps       xmm7, xmm7, dword ptr [r8], 32 
-    vblendps        xmm4, xmm7, xmm4, 8            
-    vmulps  xmm5, xmm5, xmm6
-    vaddps  xmm4, xmm5, xmm4
-    vmovss  dword ptr [rcx], xmm4
-    vextractps      dword ptr [rdx], xmm4, 1
-    vextractps      dword ptr [r8], xmm4, 2
-    vshufps xmm5, xmm4, xmm4, 255          
-    vmovss  dword ptr [r9], xmm5
-    vbroadcastss    ymm5, dword ptr [rdi + 4*rax + 48]
-    vmovss  xmm6, dword ptr [rsi + 12]    
-    vinsertps       xmm6, xmm6, dword ptr [rsi + 28], 16 
-    vinsertps       xmm6, xmm6, dword ptr [rsi + 44], 32 
-    vinsertps       xmm6, xmm6, dword ptr [rsi + 60], 48 
-    vmovss  xmm7, dword ptr [rcx]           
-    vinsertps       xmm7, xmm7, dword ptr [rdx], 16
-    vinsertps       xmm7, xmm7, dword ptr [r8], 32
-    vblendps        xmm4, xmm7, xmm4, 8            
-    vmulps  xmm5, xmm5, xmm6
-    vaddps  xmm4, xmm5, xmm4
-    vmovss  dword ptr [rcx], xmm4
-    vextractps      dword ptr [rdx], xmm4, 1
-    vextractps      dword ptr [r8], xmm4, 2
-    vextractps      dword ptr [r9], xmm4, 3
-    inc     rax
-    cmp     rax, 4
-    jne     $LL0
+$LBB0_1:
+    vbroadcastss    xmm5, dword ptr [rcx + rax]
+    vmulps  xmm5, xmm0, xmm5
+    vaddps  xmm5, xmm5, xmm4
+    vmovaps xmmword ptr [rsp + rax], xmm5
+    vbroadcastss    xmm6, dword ptr [rcx + rax + 4]
+    vmulps  xmm6, xmm1, xmm6
+    vaddps  xmm5, xmm5, xmm6
+    vmovaps xmmword ptr [rsp + rax], xmm5
+    vbroadcastss    xmm6, dword ptr [rcx + rax + 8]
+    vmulps  xmm6, xmm2, xmm6
+    vaddps  xmm5, xmm5, xmm6
+    vmovaps xmmword ptr [rsp + rax], xmm5
+    vbroadcastss    xmm6, dword ptr [rcx + rax + 12]
+    vmulps  xmm6, xmm3, xmm6
+    vaddps  xmm5, xmm5, xmm6
+    vmovaps xmmword ptr [rsp + rax], xmm5
+    add     rax, 16
+    cmp     rax, 64
+    jne     $LBB0_1
     vmovaps ymm0, ymmword ptr [rsp]
-    vmovups xmmword ptr [rdi], xmm0
+    vmovups xmmword ptr [rdx], xmm0
     vmovaps xmm0, xmmword ptr [rsp + 16]
-    vmovups xmmword ptr [rdi + 16], xmm0
+    vmovups xmmword ptr [rdx + 16], xmm0
     vmovaps ymm0, ymmword ptr [rsp + 32]
-    vmovups xmmword ptr [rdi + 32], xmm0
+    vmovups xmmword ptr [rdx + 32], xmm0
     vmovaps xmm0, xmmword ptr [rsp + 48]
-    vmovups xmmword ptr [rdi + 48], xmm0
-    
-    pop     rsi
-    pop     rdi
+    vmovups xmmword ptr [rdx + 48], xmm0
     mov     rsp, rbp
     pop     rbp
     vzeroupper
