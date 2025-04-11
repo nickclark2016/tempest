6fefeace3df78390f95bf8afbfa037153462d153
Nicholaus Clark <nicholaus.clark115@gmail.com>
2025-04-11 10:45:01 -0400

    Fix casing of GLFW include

--- projects/rhi/vk/src/window.cpp
+++ projects/rhi/vk/src/window.cpp
@@ -4,7 +4,7 @@
 
 #include <tempest/logger.hpp>
 
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 
 namespace tempest::rhi::vk
 {
--- projects/rhi/vk/src/window.hpp
+++ projects/rhi/vk/src/window.hpp
@@ -4,7 +4,7 @@
 #include <tempest/int.hpp>
 #include <tempest/string.hpp>
 
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
 
 namespace tempest::rhi::vk
